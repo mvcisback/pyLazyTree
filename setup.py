08fd28bf696215d21e43698c44dafcd1c37bdf50
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-01-13 22:54:02 -0800

    version bump

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name='lazytree',
-    version='0.1.0',
-    description='TODO',
+    version='0.2.0',
+    description='Python library for manipulating infinite trees.',
     url='https://github.com/mvcisback/pyLazyTree',
     author='Marcell Vazquez-Chanlatte',
     author_email='marcell.vc@eecs.berkeley.edu',
